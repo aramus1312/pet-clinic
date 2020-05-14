5bc12d291fb600b0b26181e65a374cdff51b5f9c
aramus <adam131291@gmail.com>
2020-05-14 23:22:28 +0200

    Start-commit

---
+++ pet-clinic2/src/main/java/aramus/springframework/petclinic/PetClinic2Application.java
@@ -0,0 +1,13 @@
+package aramus.springframework.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinic2Application.class, args);
+	}
+
+}
